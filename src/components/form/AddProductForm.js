66e7a82f7eb489439b2dc050c903eba04027652b
saharabanu <saharabanungn@gmail.com>
2024-01-06 16:27:32 +0600

    form validation done

--- src/components/form/AddProductForm.js
+++ src/components/form/AddProductForm.js
@@ -1,15 +1,20 @@
 'use client'
-
-import { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 const AddProductForm = ({ isOpen, onClose }) => {
     const {
         register,
         handleSubmit,
+        reset,
         formState: { errors },
       } = useForm();
-      const onSubmit = (data) => console.log(data)
+      const onSubmit = (data) => {
+        console.log(data);
+        toast.success("Product added successfully");
+        reset()
+        onClose()
+      }
     if (!isOpen) {
         return null;
       }
@@ -17,21 +22,32 @@ const AddProductForm = ({ isOpen, onClose }) => {
     return (
         
       
-        <dialog className="fixed top-0 left-0  flex items-center justify-center">
-          <div className="bg-globalBody p-8 rounded shadow-md">
+        <dialog className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  flex items-center justify-center ">
+          <div className="bg-globalBody p-8 rounded-lg shadow-md w-[440px]">
+          <div className="flex justify-between items-center">
+          <h4 className="py-2 text-orangePrimary">Add Product</h4>
+          <button
+              onClick={onClose}
+              className=" text-orangePrimary text-xl mb-2"
+            >
+             X
+            </button>
+          </div>
+
+           
             {/* Modal content */}
            
             <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" type="text" {...register("Name")} className="border w-full"  placeholder="Enter Product Name"/>
+      <input  type="text" {...register("Name")} className="border w-full px-2 py-3 rounded-lg outline-none"  placeholder="Enter Product Name" required/>
       <br/>  <br/>
-      <input defaultValue="test" type="text" {...register("ImageUrl")} className="border w-full"  placeholder="Enter Product Img link"/>
+      <input  type="text" {...register("ImageUrl")} className="border w-full px-2 py-3 rounded-lg outline-none"  placeholder="Enter Product Img link" required/>
       <br/>  <br/>
-      <input defaultValue="test" type="number" {...register("price")} className="border w-full"  placeholder="Enter Product Price"/>
+      <input  type="number" {...register("price")} className="border w-full px-2 py-3 rounded-lg outline-none"  placeholder="Enter Product Price" required/>
       <br/>  <br/>
       <label>IsPopular</label>
       <br/>  
-      <select {...register("IsPopular")}>
+      <select {...register("IsPopular")} className="border w-full px-2 py-3 rounded-lg outline-none" required>
         <option value="true">True</option>
         <option value="false">false</option>
        
@@ -39,7 +55,7 @@ const AddProductForm = ({ isOpen, onClose }) => {
       <br/>  <br/>
       <label>IsRecomended</label>
       <br/>  
-      <select {...register("IsRecommended")}>
+      <select {...register("IsRecommended")} className="border w-full px-2 py-3 rounded-lg outline-none" required>
       <option value="true">True</option>
         <option value="false">false</option>
       </select>
@@ -48,16 +64,17 @@ const AddProductForm = ({ isOpen, onClose }) => {
       
       {/* {errors.exampleRequired && <span>This field is required</span>} */}
 
-      <input type="submit" />
-    </form>
-
-            {/* Close button */}
-            <button
+      <input type="submit" value="Add Product" className="bg-orangePrimary text-white px-3 py-2 rounded hover:bg-yellowPrimary transition-all duration-300 delay-150"/>
+      <button
               onClick={onClose}
-              className="mt-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+              className=" mx-4 bg-gray1  hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
             >
               Close
             </button>
+    </form>
+
+            {/* Close button */}
+           
           </div>
         </dialog>
     
